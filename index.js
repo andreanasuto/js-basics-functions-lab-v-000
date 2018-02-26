088521a6a6bae0962738a760ba4011cc57630a31
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-26 23:36:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,7 @@ function distanceTravelledInFeet(starting, ending) {
 function calculatesFarePrice(start, destination) {
   if (distanceTravelledInFeet(start,destination) < 400) {
     return 0
+  } else if ((distanceTravelledInFeet(start,destination) > 2000)) {
+
   }
 }
