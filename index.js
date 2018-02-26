b7f53dd74dd90c02fa8adf67c8ef48ed98f54946
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-26 23:39:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,9 @@ function calculatesFarePrice(start, destination) {
     return 0
   } else if (distanceTravelledInFeet(start,destination) > 400 &&  distanceTravelledInFeet(start,destination) < 2000) {
     return distanceTravelledInFeet(start,destination) * 0.02
+  } else if (distanceTravelledInFeet(start,destination) > 2500) {
+    return "cannot travel that far"
+  } else {
+    return 25
   }
 }
