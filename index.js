bb52e751eeeee8dd512cb69d610c3d56da3b9235
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-26 23:31:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function distanceFromHqInBlocks(location) {
   return Math.abs(location - 42)
 }
 
-function distanceFromHqInFeet(feet) {
-  distanceFromHqInBlocks()
+function distanceFromHqInFeet(location) {
+  return distanceFromHqInBlocks(location) * 264
 }
 
 function calculatesFarePrice(start, destination) {
